437a50334221bc6fc0906319dd13b8addd09917a
Abdelrahman Mashaal <77249516+mash3al-29@users.noreply.github.com>
2024-03-17 21:30:24 +0200

    Final Quote Generator App

---
+++ Task2_QuoteGenerator/android/app/src/main/kotlin/com/codealpha/quotegenerator/quote_generator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.codealpha.quotegenerator.quote_generator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Task2_QuoteGenerator/lib/QuoteCubit/QuoteStates.dart
@@ -0,0 +1,11 @@
+abstract class Quote_States{}
+
+class Initial_State extends Quote_States{}
+
+class Update_Quote extends Quote_States{}
+
+class GenerateQuote extends Quote_States{}
+
+class QuoteError extends Quote_States{}
+
+
